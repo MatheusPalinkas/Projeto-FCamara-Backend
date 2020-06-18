661733e869186a86a676b0425a8d6235d0332c5f
Adriana Penha da Silva <dryk.ps@gmail.com>
2020-06-18 16:27:07 -0300

    Task-8: login autenticacao

---
+++ projeto/src/main/java/br/com/projetofcamara/projeto/controller/form/LoginForm.java
@@ -0,0 +1,20 @@
+package br.com.projetofcamara.projeto.controller.form;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class LoginForm {
+
+	private String email;
+	private String senha;
+		
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public UsernamePasswordAuthenticationToken converter() {
+		
+		return new UsernamePasswordAuthenticationToken(email, senha);
+	}
+}
